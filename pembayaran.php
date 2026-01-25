bc5a6de74ad0edbd566ca98cca2a5475d2092fdf
lindaaryani <122853787+lindaaryani@users.noreply.github.com>
2026-01-25 23:04:34 +0800

    Fix payment module and login page

--- pembayaran.php
+++ pembayaran.php
@@ -1,38 +1,73 @@
-<h1 class="h3 mb-3">Data Pembayaran</h1>
+<?php
+include "koneksi.php";
+?>
 
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-                <a href="?page=pembayaran_tambah" class="btn btn-primary">+ Tambah Data</a>
-                <hr>
-                <table class="table">
-                    <tr>
-                        <th>No</th>
-                        <th>Nama Pembayaran</th>
-                        <th>Kompetensi Keahlian</th>
-                        <th>Aksi</th>
-                    </tr>
-                    <?php
-                    $i = 1;
-                    $query = mysqli_query($koneksi, "SELECT*FROM pembayaran");
-                    while ($data = mysqli_fetch_array($query)) {
-                        ?>
-                        <tr>
-                            <td><?php echo $i; ?> </td>
-                            <td><?php echo $data['nama_pembayaran']; ?> </td>
-                            <td><?php echo $data['kompetensi_keahlian']; ?> </td>
-                            <td>
-                                <a href="?page=pembayaran_ubah&id=<?php echo $data['id_pembayaran']; ?>" class="btn btn-warning">Ubah</a>
-                                <a href="?page=pembayaran_hapus&id=<?php echo $data['id_pembayaran']; ?>" class="btn btn-danger">Hapus</a>
-                            </td>
-                        </tr>
-                        <?php
-                        $i++;
-                    }
-                    ?>
-                </table>
-            </div>
-        </div>
+<div class="card">
+    <div class="card-header">
+        <h5 class="card-title">Data Pembayaran</h5>
+        <a href="index.php?page=pembayaran_tambah" class="btn btn-primary">
+            + Tambah Data
+        </a>
+    </div>
+
+    <div class="card-body">
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>NISN</th>
+                    <th>Nama Siswa</th>
+                    <th>Bulan</th>
+                    <th>Tahun</th>
+                    <th>Tanggal Bayar</th>
+                    <th>Jumlah</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            <?php
+            $no = 1;
+            $query = mysqli_query($koneksi, "
+                SELECT 
+                    pembayaran.id_pembayaran,
+                    pembayaran.nisn,
+                    siswa.nama,
+                    pembayaran.bulan_dibayar,
+                    pembayaran.tahun_dibayar,
+                    pembayaran.tgl_bayar,
+                    pembayaran.jumlah_bayar
+                FROM pembayaran
+                JOIN siswa ON pembayaran.nisn = siswa.nisn
+                ORDER BY pembayaran.id_pembayaran DESC
+            ");
+
+            while ($data = mysqli_fetch_assoc($query)) {
+            ?>
+                <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= $data['nisn'] ?></td>
+                    <td><?= $data['nama'] ?></td>
+                    <td><?= $data['bulan_dibayar'] ?></td>
+                    <td><?= $data['tahun_dibayar'] ?></td>
+                    <td><?= $data['tgl_bayar'] ?></td>
+                    <td>Rp <?= number_format($data['jumlah_bayar'],0,',','.') ?></td>
+                    <td>
+                        <a href="index.php?page=pembayaran_ubah&id=<?= $data['id_pembayaran'] ?>"
+                           class="btn btn-warning btn-sm">
+                           Ubah
+                        </a>
+
+                        <a href="index.php?page=pembayaran_hapus&id=<?= $data['id_pembayaran'] ?>"
+                           onclick="return confirm('Yakin ingin menghapus data ini?')"
+                           class="btn btn-danger btn-sm">
+                           Hapus
+                        </a>
+                    </td>
+                </tr>
+            <?php } ?>
+
+            </tbody>
+        </table>
     </div>
 </div>
